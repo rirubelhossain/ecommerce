2960f9cd9c760aec2c754fb39311fb959cdabac9
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-08-05 14:09:39 +0600

    Auth Update
    login and registration updated

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route :: get('/', HomeComponent::class);
 Route :: get('cart/', CartComponent::class);
 Route :: get('checkout/', CheckoutComponent::class);
 Route :: get('shop/', ShopComponent::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
