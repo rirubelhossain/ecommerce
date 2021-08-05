1d2a5da66e953d558753005199b4f177dfb5d7e8
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-08-05 12:27:21 +0600

    n

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent ;
+use App\Http\Livewire\CartComponent ;
+use App\Http\Livewire\ShopComponent ;
+use App\Http\Livewire\CheckoutComponent ;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +25,6 @@ Route::get('/', function () {
 */
 
 Route :: get('/', HomeComponent::class);
+Route :: get('cart/', CartComponent::class);
+Route :: get('checkout/', CheckoutComponent::class);
+Route :: get('shop/', ShopComponent::class);
