5495c2a911eb2dea4ac196ab182e7113bc080418
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-08-06 23:18:26 +0600

    product and category changes

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product ;
 use Livewire\Component;
+use Livewire\WithPagination ; 
+///use App\Http\Livewire\Product ;
 
 class ShopComponent extends Component
-{
+{   
+    use WithPagination ;  
     public function render()
-    {
-        return view('livewire.shop-component')->layout('layouts.base');
+    {   
+        $products = Product::paginate(12);
+        ///$products = Product::paginate(12);
+        return view('livewire.shop-component',['products'=>$products])->layout('layouts.base');
+        
     }
 }
