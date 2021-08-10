356b5762d95d8508ac0c4830a6a00f398dfbc3f2
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-08-10 12:40:43 +0600

    Delete Cart Iteam
    clear all cart or specific by cart.

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -7,18 +7,35 @@ use Cart;
 
 class CartComponent extends Component
 {   
+    //Cart Quantity Increase
     public function increaseQuantity($rowId)
     {
         $product = Cart::get($rowId) ;
         $qty = $product->qty + 1 ;
         Cart::update($rowId , $qty );
     }
+
+    //Cart Quantity Decrease
     public function decreaseQuantity($rowId)
     {
         $product = Cart::get($rowId) ;
         $qty = $product->qty - 1 ;
         Cart::update($rowId , $qty );
     }
+
+    //Delete Cart Iteam
+    public function destroy($rowId)
+    {
+        Cart::remove($rowId) ;
+        session()->flash('success_message','Iteam has been removed') ;
+    }
+
+    ///Delete all cart
+    public function destroyAll()
+    {
+        Cart::destroy();
+    }
+
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
